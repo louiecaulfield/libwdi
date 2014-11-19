504ea4baadc37d029cd44c06f00d84f1624b83b8
Jasper van Bourgognie <van.bourgognie@gmail.com>
2014-11-19 01:38:11 +0100

    Smartscope specifics

--- msvc/config.h
+++ msvc/config.h
@@ -21,7 +21,7 @@
  * match your WinUSB redist directrories
  */
 #ifndef DDK_DIR
-#define DDK_DIR "C:/Program Files (x86)/Windows Kits/8.1"
+#define DDK_DIR "C:/Program Files/Windows Kits/8.1"
 #endif
 
 /* DDK WDF coinstaller version */
@@ -35,12 +35,12 @@
 
 /* embed libusb0 driver files from the following location */
 #ifndef LIBUSB0_DIR
-#define LIBUSB0_DIR "D:/libusb-win32"
+//#define LIBUSB0_DIR/ "D:/libusb-win32"
 #endif
 
 /* embed libusbK driver files from the following location */
 #ifndef LIBUSBK_DIR
-#define LIBUSBK_DIR "D:/libusbK/bin"
+//#define LIBUSBK_DIR "D:/libusbK/bin"
 #endif
 
 /* embed user defined driver files from the following location */
